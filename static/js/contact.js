8db26db1f097c3db08bbabd46b8b21ee7f2bd26b
Jonathan Kaplan <github@jonathan-kaplan.eu>
2025-11-05 23:30:02 +0000

    Add reCAPTCHA to contact form

--- static/js/contact.js
+++ static/js/contact.js
@@ -1,19 +1,20 @@
-// contact.js
-
 const btn = document.getElementById("button");
 const form = document.getElementById("form");
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
-
   btn.value = "Sending...";
 
-  // Get the reCAPTCHA token from the widget
-  const recaptchaToken = grecaptcha.getResponse();
+  // Get the reCAPTCHA token from the first widget
+  const recaptchaWidget = document.querySelector(".g-recaptcha");
+  const recaptchaResponse = grecaptcha.getResponse(
+    recaptchaWidget.getAttribute("data-widget-id")
+      ? parseInt(recaptchaWidget.getAttribute("data-widget-id"))
+      : 0
+  );
 
-  // If user didn’t complete reCAPTCHA
-  if (!recaptchaToken) {
-    alert("Please confirm you’re not a robot.");
+  if (!recaptchaResponse) {
+    alert("Please check the reCAPTCHA box before sending.");
     btn.value = "Send Email";
     return;
   }
@@ -23,21 +24,20 @@ form.addEventListener("submit", function (event) {
 
   emailjs
     .sendForm(serviceID, templateID, this, {
-      publicKey: "hcwh1BzimepUo_Ba4", // your EmailJS public key
-      "g-recaptcha-response": grecaptcha.getResponse(), // send token to EmailJS
+      publicKey: "hcwh1BzimepUo_Ba4",
     })
     .then(
       () => {
         btn.value = "Send Email";
         alert("Message sent!");
         form.reset();
-        grecaptcha.reset(); // reset reCAPTCHA for next submission
+        grecaptcha.reset();
       },
       (err) => {
         btn.value = "Send Email";
         alert("Error: " + JSON.stringify(err));
         grecaptcha.reset();
-      },
+      }
     );
 });
 
